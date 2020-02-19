a4fcfbc0d299d0c293ca90b330dd2f3e625aeb01
Chad Sutherland <46322498+ChadSutherland@users.noreply.github.com>
2020-02-18 19:35:05 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-cd mysite/
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --no-input
-
-
-gsutil rsync -R static/ gs://bhcc-csx.appspot.com/static
-gcloud app deploy --quiet
