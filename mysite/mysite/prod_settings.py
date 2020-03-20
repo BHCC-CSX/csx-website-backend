c2be52c47a50eceadcd180e4dc7993c8bdb8af28
ChadSutherland <csuth1@live.com>
2020-03-20 17:31:36 -0400

    Change static file and database config for heroku

--- mysite/mysite/prod_settings.py
+++ mysite/mysite/prod_settings.py
@@ -1,31 +1,9 @@
 import os
+import dj_database_url
 
 DEBUG = False
 
-
 SECRET_KEY = os.environ['CSX_SECRET_KEY']
 
-# SECURE_SSL_REDIRECT = True
-# SECURE_CONTENT_TYPE_NOSNIFF = True
-# SECURE_BROWSER_XSS_FILTER = True
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
-# X_FRAME_OPTIONS = 'DENY'
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# Running on production App Engine, so connect to Google Cloud SQL using
-# the unix socket at /cloudsql/<your-cloudsql-connection string>
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': '/cloudsql/csx-backend:us-east1:csxdb-instance',
-        'NAME': 'csxdb',
-        'USER': os.environ['CSXDB_USER'],
-        'PASSWORD': os.environ['CSXDB_PASSWORD']
-    }
-}
-
-DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = 'csx-backend.appspot.com'
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
--- mysite/mysite/shared_settings.py
+++ mysite/mysite/shared_settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-if os.getenv('GAE_INSTANCE', None):
+if 'PROD_ENV' in os.environ:
     from .prod_settings import *
 else:
     from .dev_settings import *
@@ -107,9 +108,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = 'static'
-STATIC_URL = 'https://storage.googleapis.com/bhcc-csx.appspot.com/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATICFILES_DIRS = [
-    "./mysite/static/"
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+django_heroku.settings(locals())
