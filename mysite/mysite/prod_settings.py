cb953e96743042cf314efd441071cdccbf912d6a
ChadSutherland <csuth1@live.com>
2020-03-20 17:43:20 -0400

    Fix secret key env var name

--- mysite/mysite/prod_settings.py
+++ mysite/mysite/prod_settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 
 DEBUG = False
 
-SECRET_KEY = os.environ['CSX_SECRET_KEY']
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
