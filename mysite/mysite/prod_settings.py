d666aad26cc9e402b80dd6aca2ac558159301feb
ChadSutherland <csuth1@live.com>
2020-03-21 10:41:03 -0400

    Switch storage from Google Cloud Storage to AWS S3

--- mysite/mysite/prod_settings.py
+++ mysite/mysite/prod_settings.py
@@ -3,7 +3,28 @@ import dj_database_url
 
 DEBUG = False
 
+# Secret key
+
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+# Database
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+# Storage
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# S3 static storage
+STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'mysite.storage_backends.StaticStorage'
+
+# S3 media storage
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.PublicMediaStorage'
