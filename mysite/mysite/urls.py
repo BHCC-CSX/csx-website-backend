f19b50d4078b5d727282be2e0192069faa7d9802
Col Crunch <rhartnett35@gmail.com>
2020-03-22 14:58:19 -0400

    Update URLs to include use of NamedURLHook, as well as to make use of the swagger documentation. (Swagger documentation is how hosted at the root url of the site)

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .hooks import get_hooks
 
 router = routers.DefaultRouter()
@@ -29,10 +32,32 @@ for app in apps:
     # print(app().reg.registry)
     api_urls += app().pats
 
+apps = get_hooks("named_url_hook")
+for app in apps:
+    app_urls = app().pats
+    api_urls += [path(app().prefix, include(app_urls))]
+
+
+# Swagger Stuff
+schema_view = get_schema_view(
+   openapi.Info(
+      title="CSX Backend API",
+      default_version='v1',
+      description="This API serves as the backend for the BHCC CSX website.",
+      contact=openapi.Contact(email="contact@bhcsx.tech"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+    url(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('api/', include(api_urls))
+    path('api/', include(api_urls)),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 static_url = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) if settings.MEDIA_URL else None
 
