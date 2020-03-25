16ae9189cffdfe921e61bbc8c0d38c6ec8caa2fc
Col Crunch <rhartnett35@gmail.com>
2020-03-25 12:39:08 -0400

    Add admin actions to approve or disapprove posts.

--- mysite/projects/admin.py
+++ mysite/projects/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from projects.models import Project
 
+
 # Register your models here.
+def approve(modeladmin, request, queryset):
+    queryset.update(is_approved=True)
+
+
+def deny(modeladmin, request, queryset):
+    queryset.update(is_approved=False)
+
+
+approve.short_description = "Approve Projects"
+deny.short_description = "Disapprove Projects"
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    actions = [approve, deny]
 
 
 admin.site.register(Project, ProjectAdmin)
