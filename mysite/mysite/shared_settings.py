86541bc18e534f4bc44b43c30e96b4b6c5f64bc1
Col Crunch <rhartnett35@gmail.com>
2020-04-23 03:53:54 -0400

    Remove OAuth settings, add WebUI settings to allow for use of JWT when interacting with the API WebUI, disable session auth as it is incompatible with JWT auth.

--- mysite/mysite/shared_settings.py
+++ mysite/mysite/shared_settings.py
@@ -94,11 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-OAUTH2_PROVIDER = {
-    # this is the list of available scopes
-    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
-}
-
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
@@ -109,6 +104,17 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     )
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'JWT': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
 }
 
 SIMPLE_JWT = {
